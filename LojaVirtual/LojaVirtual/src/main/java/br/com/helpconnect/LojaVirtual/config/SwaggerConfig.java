e4c640c17d3cde3ab48cee6b18aabe7e062a5c5e
Clamant96 <kevin.lazzarotto@hotmail.com>
2021-04-25 18:11:46 -0300

    Criação de deploy de projeto e documentação por meio do Swagger.

---
+++ LojaVirtual/LojaVirtual/src/main/java/br/com/helpconnect/LojaVirtual/config/SwaggerConfig.java
@@ -0,0 +1,49 @@
+package br.com.helpconnect.LojaVirtual.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+	
+	@Bean
+	public Docket docket() {
+		return new Docket(DocumentationType.SWAGGER_2)
+			.select()
+			.apis( RequestHandlerSelectors.basePackage
+			("br.com.helpconnect.LojaVirtual.controller") )
+			.paths(PathSelectors.any())
+			.build()
+			.apiInfo(apiInfo());	
+		
+	}
+	
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+			.title("API - Loja Virtual")
+			.description("API para compor aplicações WEB Spring/Java/MySQL")
+			.version("1.0")
+			.contact(contact())
+			.build();
+		
+	}
+	
+	private Contact contact() {
+		return new Contact("Kevin Alec Neri Lazzarotto",
+			"https://github.com/Clamant96",
+			"Estudante em ADS | Desenvolvedor Web Java Junior Full Stack");
+		
+	}
+
+
+}
