a5581b726811213480c9118a7addc1fcf787df7f
Kevin Alec Neri Lazzarotto <kevin.lazzarotto@hotmail.com>
2022-06-18 14:33:17 -0300

    Ajuste de delecao de produtos do carrinho do usuario, com o ajuste do total de carrinho ajustado de acordo com o produto deletado

--- LojaVirtual/LojaVirtual/src/main/java/br/com/helpconnect/LojaVirtual/service/ProdutoService.java
+++ LojaVirtual/LojaVirtual/src/main/java/br/com/helpconnect/LojaVirtual/service/ProdutoService.java
@@ -25,108 +25,6 @@ public class ProdutoService {
 	
 	double a = 0;
 	int posicao = 0; // aramazena a posicao do item dentro do array de lista de desejos
-	
-	/*public Produto compraProduto(long idProduto, long idCliente) {
-			
-		Optional<Produto> produtoExistente = produtoRepository.findById(idProduto);
-		Optional<Cliente> clienteExistente = clienteRepository.findById(idCliente);
-		
-		if(produtoExistente.isPresent() && clienteExistente.isPresent() && produtoExistente.get().getEstoque() == 0) {
-			// ACRESCENTA MAIS 50 PRODUTOS AO ESTOQUE 
-			produtoExistente.get().setEstoque(50);
-			
-		}
-		
-		// SE O 'PRODUTO' E 'PEDIDO' EXISTIREM, E SE O 'ESTOQUE' CONTEM PRODUTOS DISPONIVEIS ENTRA NA CONDICAO
-		if(produtoExistente.isPresent() && clienteExistente.isPresent() && produtoExistente.get().getEstoque() >= 0 && !(clienteExistente.get().getPedidos().isEmpty())) {
-			
-			// ADICIONA O PRODUTO AO CARRINHO DO USUARIO 
-			produtoExistente.get().getPedidos().add(clienteExistente.get());
-			
-			System.out.println("Retorno: "+ clienteExistente.get().getPedidos().contains(produtoExistente.get()));
-			
-			System.out.println("QTD produtos "+ clienteExistente.get().getPedidos().size());
-			
-			// ARMAZENA A QTD DE PRODUTOS
-			int contador = 0;
-			
-			// ARMAZENA OS IDs DOS PRODUTOS LISTADOS DENTRO DO CARRINHO DO USUARIO
-			long[] vetor = new long[clienteExistente.get().getPedidos().size()];
-			
-			for(int i = 0; i < clienteExistente.get().getPedidos().size(); i++) {
-				
-				vetor[i] = clienteExistente.get().getPedidos().get(i).getId();
-				
-				System.out.println("Posicao do vetor ["+ i +"] = "+ vetor[i]);
-				System.out.println("Produto ID: "+ produtoExistente.get().getId());
-				
-				if(vetor[i] == produtoExistente.get().getId()) {
-					contador++;
-					
-				}
-				
-			}
-			
-			System.out.println("Valor Total a Pagar ATUAL "+ clienteExistente.get().getValorTotal());
-			
-			// RETIRA O VALOR EXISTENTE DO CARRINHO PARA PODER SER RECALCULADO 
-			clienteExistente.get().setValorTotal(clienteExistente.get().getValorTotal() - (produtoExistente.get().getPreco() * contador));
-			
-			// COMPENSA ACRESCENTANDO O NOVO PRODUTO AO CARRINHO ==> O ID INFORMADO 
-			contador++;
-			System.out.println("QTD de produto: "+ contador);
-			
-			// INSERE O VALOR DO CONTADOR == QTD DE PRODUTOS POR ID 
-			produtoExistente.get().setQtdPedidoProduto(contador);
-			// DEBITA O ESTOQUE SEMPRE QUE E INSERIDO UM PRODUTO A UM CARRINHO 
-			produtoExistente.get().setEstoque(produtoExistente.get().getEstoque() - 1);
-			
-			System.out.println("Valor Total a Pagar ZERADO "+ clienteExistente.get().getValorTotal());
-			
-			// AJUSTA O VALOR DO CARRINHO DE UM USUARIO ESPECIFICO 
-			clienteExistente.get().setValorTotal(clienteExistente.get().getValorTotal() + (produtoExistente.get().getPreco() * produtoExistente.get().getQtdPedidoProduto()));
-			
-			System.out.println("Valor Total a Pagar ATUALIZADO "+ clienteExistente.get().getValorTotal());
-			
-			System.out.println("Contador: "+ contador);
-			System.out.println("QTD Produtos Comprados: "+ produtoExistente.get().getQtdPedidoProduto());
-			System.out.println("Valor Produto: "+ produtoExistente.get().getPreco());
-			System.out.println("Valor Carrinho: "+ clienteExistente.get().getValorTotal());
-			
-			System.out.println("IF");
-			System.out.println("ID: "+ clienteExistente.get().getId() +" | Valor a pagar: "+ clienteExistente.get().getValorTotal());
-			
-			produtoRepository.save(produtoExistente.get());
-			clienteRepository.save(clienteExistente.get());
-			clienteRepository.save(clienteExistente.get()).getValorTotal();
-			
-			return produtoRepository.save(produtoExistente.get());
-			
-		}else if(produtoExistente.isPresent() && clienteExistente.isPresent()) {
-			// ADICIONA O PRODUTO AO CARRINHO DO USUARIO
-			produtoExistente.get().getPedidos().add(clienteExistente.get());
-			
-			// ADICIONA UM PRODUTO AO QTD PRODUTOS DENTRO DE PRODUTO 
-			produtoExistente.get().setQtdPedidoProduto(1);
-			// GERENCIA O ESTOQUE DEBITNADO UM PRODUTO DO ESTOQUE 
-			produtoExistente.get().setEstoque(produtoExistente.get().getEstoque() - 1);
-			
-			// ATUALIZA O VALOR DO CARRINHO DO USUARIO 
-			clienteExistente.get().setValorTotal(clienteExistente.get().getValorTotal() + (produtoExistente.get().getPreco() * produtoExistente.get().getQtdPedidoProduto()));
-			
-			System.out.println("ELSE");
-			System.out.println("ID: "+ clienteExistente.get().getId() +" | Valor a pagar: "+ clienteExistente.get().getValorTotal());
-			
-			produtoRepository.save(produtoExistente.get());
-			clienteRepository.save(clienteExistente.get());
-			clienteRepository.save(clienteExistente.get()).getValorTotal();
-			
-			return produtoRepository.save(produtoExistente.get());
-			
-		}
-		
-		return null;
-	}*/
 
 	public Produto compraProduto(long idProduto, long idCliente) {
 			
@@ -251,21 +149,21 @@ public class ProdutoService {
 	}
 	
 	/* DELETAR OBJETOS DO PRODUTO */
-	public Produto deletarProduto(long idProduto, long idCliente) {
+	/*public Produto deletarProduto(long idProduto, long idCliente) {
 
 		Optional<Produto> produtoExistente = produtoRepository.findById(idProduto);
 		Optional<Cliente> clienteExistente = clienteRepository.findById(idCliente);
 		
 		if(clienteExistente.get().getPedidos().contains(produtoExistente.get())) {
-			/* REMOVE O CARRINHO DO PRODUTO */
+			// REMOVE O CARRINHO DO PRODUTO 
 			produtoExistente.get().getPedidos().remove(clienteExistente.get());
 			
-			/* GERENCIA O ESTOQUE DEBITNADO UM PRODUTO DO ESTOQUE */
+			// GERENCIA O ESTOQUE DEBITNADO UM PRODUTO DO ESTOQUE 
 			produtoExistente.get().setEstoque(produtoExistente.get().getEstoque() + 1);
 			
 			int contador = 0;
 			
-			/* ARMAZENA OS IDs DOS PRODUTOS LISTADOS DENTRO DO CARRINHO DO USUARIO */
+			// ARMAZENA OS IDs DOS PRODUTOS LISTADOS DENTRO DO CARRINHO DO USUARIO 
 			long[] vetor = new long[clienteExistente.get().getPedidos().size()];
 			
 			for(int i = 0; i < clienteExistente.get().getPedidos().size(); i++) {
@@ -284,7 +182,7 @@ public class ProdutoService {
 			
 			produtoExistente.get().setQtdPedidoProduto(contador - 1);
 			
-			/* AJUSTA O VALOR DO CARRINHO DE UM USUARIO ESPECIFICO */
+			// AJUSTA O VALOR DO CARRINHO DE UM USUARIO ESPECIFICO 
 			clienteExistente.get().setValorTotal(clienteExistente.get().getValorTotal() - produtoExistente.get().getPreco());
 			
 			a = clienteExistente.get().getValorTotal();
@@ -303,7 +201,74 @@ public class ProdutoService {
 		}
 		
 		return null;
+	}*/
+
+	/* ===================================== */
+
+	public Produto deletarProduto(long idProduto, long idCliente) {
+			
+		Optional<Produto> produtoExistente = produtoRepository.findById(idProduto);
+		Optional<Cliente> clienteExistente = clienteRepository.findById(idCliente);
+		
+		// SE O 'PRODUTO' E 'PEDIDO' EXISTIREM, E SE O 'ESTOQUE' CONTEM PRODUTOS DISPONIVEIS ENTRA NA CONDICAO
+		if(clienteExistente.get().getPedidos().contains(produtoExistente.get())) {
+
+			int contadorProduto = 0;
+			
+			// NAVEGA NO ARRAY DE PRODUTO DO USUARIO, E CASO ELE SEJA REPEDIDO, E INCREMENTADO O VALOR NA QTDPRODUTO E ADICIONADO AO ARRAY
+			// CASO ELE NAO EXISTA AINDA NO ARRAY
+			for (Produto produto : clienteExistente.get().getPedidos()) {
+
+				for (Produto produtoRepeticao : clienteExistente.get().getPedidos()) {
+
+					// VERIFICA SE EXISTE REPETICAO DO PRODUTO, PARA REALIZAR O INCREMENTO DO VALOR AO 'QTDPRODUTO'
+					if(produto.getId() == produtoRepeticao.getId()) {
+							
+						contadorProduto = contadorProduto + 1;
+						
+						produto.setQtdPedidoProduto(contadorProduto); // INCREMENTA O VALOR NO CONTADOR
+
+						// GERENCIA O ESTOQUE DEBITNADO UM PRODUTO DO ESTOQUE 
+						produtoExistente.get().setQtdPedidoProduto(contadorProduto);
+
+					}
+
+				}
+
+				contadorProduto = 0;
+
+			}
+
+			// REMOVE O CARRINHO DO PRODUTO
+			produtoExistente.get().getPedidos().remove(clienteExistente.get());
+			clienteExistente.get().getPedidos().remove(produtoExistente.get()); // DELETA O PRODUTO DA LISTA DO CLIENTE DA INSTANCIA ATUAL
+
+			// GERENCIA O ESTOQUE DEBITNADO UM PRODUTO DO ESTOQUE
+			produtoExistente.get().setEstoque(produtoExistente.get().getEstoque() + 1);
+
+			System.out.println("VALOR CARRINHO: "+ clienteExistente.get().getValorTotal());
+
+			System.out.println("CONTADOR: "+ produtoExistente.get().getQtdPedidoProduto());
+			System.out.println("VALOR PRODUTO = "+ produtoExistente.get().getPreco() +" x CONTADOR = "+ 1); 
+			System.out.println("SUBTRACAO APLICADA PRECO PRODUTO X CONTADOR: "+ produtoExistente.get().getPreco() * 1);
+
+			// AJUSTA O VALOR DO CARRINHO DO USUARIO
+			double total = clienteExistente.get().getValorTotal() - (produtoExistente.get().getPreco() * 1); // TEM SOMENTE UMA UNIDADE, POIS ESTAMOS REALIZANDO A DELETA DE SOMENTE UAMA UNIDADE CABECAO
+			NumberFormat formatter = new DecimalFormat("#0.00");
+			clienteExistente.get().setValorTotal(Double.parseDouble(formatter.format(total).replace(",", ".")));
+
+			produtoRepository.save(produtoExistente.get());
+			clienteRepository.save(clienteExistente.get());
+			clienteRepository.save(clienteExistente.get()).getValorTotal();
+			
+			return produtoRepository.save(produtoExistente.get());
+			
+		}
+		
+		return null;
 	}
+
+	/* ===================================== */
 	
 	/* ADICIONA UM PRODUTO ESPECIFICO A LISTA DE DESEJOS DO USUARIO */
 	public Produto adicionarProdutoListaDeDesejo(long idProduto, long idCiente) {
